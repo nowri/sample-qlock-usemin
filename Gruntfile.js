86e2a1dcb06954f98ee4bc8b1bb3720656b61c73
ka <interactive@nowri.in>
2014-12-02 23:24:28 +0900

    Update

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,63 +3,62 @@ module.exports = function(grunt) {
 	'use strict';
 
 
+	grunt.initConfig({
 
-grunt.initConfig({
+		/*
+		 * 初期設定
+		 * */
+		dirs:{
+			deploy:'deploy',
+			release:'_release',
+			src:'src'
+		},
 
-/*
-* 初期設定
-* */
-dirs	: {
-deploy	:	'deploy',
-release	:	'_release',
-src		:	'src'
-},
+		pkg:grunt.file.readJSON('package.json'),
 
-pkg		:	grunt.file.readJSON('package.json'),
+		copy:{
+			toRelease:{
+				files:[{
+					expand:true,
+					cwd:'<%= dirs.deploy %>',
+					src:['**'],
+					dest:'<%= dirs.release %>'
+				}],
+				dot:true
+			}
+		},
 
-copy	:	{
-toRelease : {
-	files : [{
-		expand : true,
-		cwd : '<%= dirs.deploy %>',
-		src : ['**'],
-		dest : '<%= dirs.release %>'
-	}],
-	dot : true
-}
-},
+		clean:{
+			allRelease:['<%= dirs.release %>/**']
+		},
 
-clean : {
-allRelease : ['<%= dirs.release %>/**']
-},
+		watch:{
+			deploy:{
+				files:['<%= dirs.src %>/**'],
+				tasks:['deploy']
+			},
 
-watch : {
-deploy : {
-	files : ['<%= dirs.src %>/**'],
-	tasks : ['deploy']
-},
+			release:{
+				files:['<%= dirs.src %>/**'],
+				tasks:['release']
+			}
+		},
 
-release : {
-	files : ['<%= dirs.src %>/**'],
-	tasks : ['release']
-}
-},
+		/*
+		 * カスタム設定
+		 * */
+		useminPrepare:{
+			html:'<%= dirs.deploy %>/index.html',
+			options:{
+				root:'<%= dirs.deploy %>/',
+				dest:'<%= dirs.release %>/'
+			}
+		},
+		usemin:{
+			html:'<%= dirs.release %>/index.html'
+		}
 
-/*
-* カスタム設定
-* */
-useminPrepare:{
-html:'<%= dirs.deploy %>/index.html',
-options:{
-	root:'<%= dirs.deploy %>/',
-	dest:'<%= dirs.release %>/'
-}
-},
-usemin:{
-html:'<%= dirs.release %>/index.html'
-}
-
-});
+	});
 
 	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-contrib-clean');
@@ -68,9 +67,8 @@ html:'<%= dirs.release %>/index.html'
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-usemin');
 
-	grunt.registerTask('commonBefore', [/* ここに共通タスクネーム記述 */]);
-	grunt.registerTask('deploy', ['commonBefore' /* ここにdeploy専用タスクネーム記述 */]);
-	grunt.registerTask('release', ['commonBefore', 'clean:allRelease', 'copy:toRelease','useminPrepare', 'concat:generated', 'uglify:generated', 'usemin']);
+	grunt.registerTask('deploy', []);
+	grunt.registerTask('release', ['clean:allRelease', 'copy:toRelease', 'useminPrepare', 'concat:generated', 'uglify:generated', 'usemin']);
 	grunt.registerTask('default', ['release']);
 
 };
